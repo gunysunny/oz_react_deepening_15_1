5a2288a5348341fc9a8abc64d1d233d4f1884ad3
gunysunny <mikan31@naver.com>
2025-06-09 15:21:02 +0900

    react recoil과제제출

--- src/components/ControllerDetailModal.jsx
+++ src/components/ControllerDetailModal.jsx
@@ -1,26 +1,31 @@
 import React from 'react';
+import { useSetRecoilState } from 'recoil';
+import { dataState } from '../recoil/atoms';
 
 const ControllerDetailModal = ({ onClose }) => {
   // recoil을 이용하여 상태관리를 하도록 변경합니다.
   // useSetRecoilState를 이용하여 전역상태를 업데이트하도록 수정합니다.
   // handleForm에서 data를 만들고 있습니다. 이 data를 할당해야합니다.
+  const setData = useSetRecoilState(dataState);
 
   const handleForm = (e) => {
     e.preventDefault();
 
     const formData = new FormData(e.target);
-    const data = {
+    const newItem = {
       id: Date.now(),
       type: formData.get('type'),
       title: formData.get('title'),
       desc: formData.get('desc'),
       created_at: new Date().toISOString().split('T')[0],
     };
+
+    setData(prev => [...prev, newItem]);
     onClose();
   };
   return (
     <div onClick={onClose} className="fixed inset-0 flex items-center justify-center bg-black/70 bg-opacity-50 z-50">
-      <div onClick={(e) => e.stopPropagation()} className="bg-white rounded-lg shadow-lg p-6 w-[600px]">
+      <div onClick={e => e.stopPropagation()} className="bg-white rounded-lg shadow-lg p-6 w-[600px]">
         <h2 className="text-xl font-semibold mb-4 whitespace-break-spaces">업무 추가</h2>
         <form onSubmit={(e) => handleForm(e)} className="flex flex-col gap-2">
           <div>업무 분류</div>
@@ -47,16 +52,8 @@ const ControllerDetailModal = ({ onClose }) => {
             required
           ></textarea>
           <div className="flex items-center gap-4">
-            <button
-              type="submit"
-              className="cursor-pointer mt-4 bg-black text-white px-4 py-2 rounded hover:bg-stone-600"
-            >
-              추가
-            </button>
-            <button
-              onClick={onClose}
-              className="cursor-pointer mt-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
-            >
+            <button type="submit" className="bg-black text-white px-4 py-2 rounded">추가</button>
+            <button type="button" onClick={onClose} className="bg-blue-500 text-white px-4 py-2 rounded">
               닫기
             </button>
           </div>
